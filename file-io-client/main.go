c5b18634bc127ab43971f8711541621761d12170
Hwanwoong <hwkim2966@gmail.com>
2024-07-05 10:09:13 +0900

    Update : Merge QUIC Ex, File-IO

--- file-io-client/main.go
+++ file-io-client/main.go
@@ -1,120 +1,47 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
-	"io"
 	"log"
-	"math/big"
+	"net/http"
 
 	"github.com/quic-go/quic-go"
 )
 
-const addr = "localhost:4242"
-
-const message = "foobar"
-
-// We start a server echoing data on the first stream the client opens,
-// then connect with a client, send the message, and wait for its receipt.
 func main() {
-	go func() { log.Fatal(echoServer()) }()
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true, // testing only
+		NextProtos:         []string{"h3", "http/1.1"},
+	}
+	url := "localhost:8080"
+	req, _ := http.NewRequest("GET", url, nil)
+	var buf bytes.Buffer
+	req.Write(&buf)
 
-	err := clientMain()
+	ctx := context.Background()
+	connection, err := quic.DialAddr(ctx, url, tlsConfig, nil)
 	if err != nil {
-		panic(err)
+		println(err.Error())
+		return
 	}
-}
 
-// Start a server that echos all data on the first stream opened by the client
-func echoServer() error {
-	listener, err := quic.ListenAddr(addr, generateTLSConfig(), nil)
+	stream, err := connection.AcceptStream(context.Background())
 	if err != nil {
-		return err
-	}
-	defer listener.Close()
-
-	conn, err := listener.Accept(context.Background())
-	if err != nil {
-		return err
+		println(err.Error())
+		return
 	}
 
-	stream, err := conn.AcceptStream(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	defer stream.Close()
+	buffer := make([]byte, 8)
 
-	// Echo through the loggingWriter
-	_, err = io.Copy(loggingWriter{stream}, stream)
-	return err
-}
+	for {
+		numBytes, err := stream.Read(buffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+		println(numBytes)
 
-func clientMain() error {
-	tlsConf := &tls.Config{
-		InsecureSkipVerify: true,
-		NextProtos:         []string{"quic-echo-example"},
-	}
-	conn, err := quic.DialAddr(context.Background(), addr, tlsConf, nil)
-	if err != nil {
-		return err
-	}
-	defer conn.CloseWithError(0, "")
-
-	stream, err := conn.OpenStreamSync(context.Background())
-	if err != nil {
-		return err
-	}
-	defer stream.Close()
-
-	fmt.Printf("Client: Sending '%s'\n", message)
-	_, err = stream.Write([]byte(message))
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, len(message))
-	_, err = io.ReadFull(stream, buf)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Client: Got '%s'\n", buf)
-
-	return nil
-}
-
-// A wrapper for io.Writer that also logs the message.
-type loggingWriter struct{ io.Writer }
-
-func (w loggingWriter) Write(b []byte) (int, error) {
-	fmt.Printf("Server: Got '%s'\n", string(b))
-	return w.Writer.Write(b)
-}
-
-// Setup a bare-bones TLS config for the server
-func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"quic-echo-example"},
+		log.Printf("Received: %s\n", buffer[:numBytes])
 	}
 }
--- file-io/main.go
+++ file-io/main.go
@@ -5,9 +5,73 @@ import (
 	"encoding/binary"
 	"fmt"
 	"os"
+	"time"
+
+	"crypto/tls"
+	"log"
+
+	"github.com/quic-go/quic-go"
+	"golang.org/x/net/context"
 )
 
 func main() {
+	cert, err := tls.LoadX509KeyPair("/Users/hwanwoong/minica.pem", "/Users/hwanwoong/minica-key.pem")
+	if err != nil {
+		log.Fatal("Error loading certificate:", err)
+	}
+	tlsConfig := &tls.Config{
+
+		Certificates:       []tls.Certificate{cert},
+		InsecureSkipVerify: true,                       // For testing purposes only
+		NextProtos:         []string{"h3", "http/1.1"}, // Enable QUIC and HTTP/3
+	}
+	listener, err := quic.ListenAddr("localhost:8080", tlsConfig, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	log.Println("QUIC server started on localhost:8080")
+
+	for {
+		connection, err := listener.Accept(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleSession(connection)
+	}
+}
+
+func handleSession(connection quic.Connection) {
+	println("New connection established:", connection.RemoteAddr().String())
+
+	ctx := context.Background()
+
+	for {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+		stream, err := connection.OpenUniStreamSync(ctx)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		go handleRequest(stream)
+	}
+}
+
+func handleRequest(stream quic.SendStream) {
+
+	for {
+		file_bin := read_file()
+		_, err := stream.Write(file_bin)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = stream.Close()
+	}
+}
+
+func read_file() []byte {
 	var total_file []byte
 	filepath := []string{"./grpc_structure.txt", "./test_restoring.txt"}
 
@@ -25,12 +89,12 @@ func main() {
 				file[i], err = os.Create(path)
 				if err != nil {
 					fmt.Println("Error creating file:", err)
-					return
+					return make([]byte, 0)
 				}
 				defer file[i].Close()
 			} else {
 				fmt.Println("Error opening file:", err)
-				return
+				return make([]byte, 0)
 			}
 		} else {
 			defer file[i].Close()
@@ -39,7 +103,7 @@ func main() {
 			fileInfo, err := file[i].Stat()
 			if err != nil {
 				fmt.Println("Error getting file information:", err)
-				return
+				return make([]byte, 0)
 			}
 			fileSize := fileInfo.Size()
 
@@ -50,7 +114,7 @@ func main() {
 			_, err = file[i].Read(data)
 			if err != nil {
 				fmt.Println("Error reading file:", err)
-				return
+				return make([]byte, 0)
 			}
 
 			file_padding := make([]byte, 512)
@@ -63,20 +127,22 @@ func main() {
 			if err != nil {
 				fmt.Println("binary read failed:", err)
 			}
-			fmt.Println(file_bin)
-			fmt.Println(file_padding)
+			//fmt.Println(file_bin)
+			//fmt.Println(file_padding)
 
 			total_file = append(total_file, file_padding...)
 
 			// Process the binary data here
-			fmt.Printf("Read %d bytes from file:\n%s\n", fileSize, data)
+			fmt.Printf("Read %d bytes from file:\n", fileSize)
 		}
 	}
 
-	fmt.Println("Total file binary sum is ... ")
-	fmt.Println(total_file)
+	//fmt.Println("Total file binary sum is ... ")
+	//fmt.Println(total_file)
 
 	for _, file := range file {
 		file.Close()
 	}
+
+	return total_file
 }
